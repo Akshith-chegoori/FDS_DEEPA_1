c3a4c4207ad1f0b49f87511129a72a30736d84db
vydhikreddy <vydhikreddy12@gmail.com>
2026-03-27 15:52:34 +0530

    11 question

--- 11.py
+++ 11.py
@@ -2,7 +2,7 @@ p=[100,250,80,430,150]
 new_p=[]
 for i in p:
     if i>200:
-        new_p.append(i * 0.9)
+         new_p.append(i * 0.9)
     else:       
-        new_p.append(i * 0.95)
+         new_p.append(i * 0.95)
 print(new_p)
