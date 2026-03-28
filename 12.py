7c94e4668d8ae52ce7d239ff8c822c08bdb377c4
CHegoori Akshith <akshithchegoori15@gmail.com>
2026-03-28 19:58:41 +0530

    12qqqqqqqq

--- 12.py
+++ 12.py
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
 nums = [2, 5, 1, 8, 3, 9, 4, 7, 6]
-rma=[]
+rmaxi=[]
 rmi=[]
 ma=0
 mi=10000
@@ -9,26 +8,7 @@ for i in nums:
         ma=i
     if i <= mi:
         mi=i
-    rma.append(ma)
+    rmaxi.append(ma)
     rmi.append(mi)
-print(rma)
+print(rmaxi)
 print(rmi)
-=======
-n=[2,5,1,8,3,9,4,7,6]
-max=[]
-maximum=n[0]
-for i in n:
-    if i>maximum:
-        maximum=i
-    max.append(maximum)
-
-min=[]
-minimum=n[0]
-for i in n:
-    if i<minimum:
-        minimum=i
-    min.append(minimum)
-
-print("running_max  = ",max)
-print("running_min = ",min)
->>>>>>> c3a4c4207ad1f0b49f87511129a72a30736d84db
